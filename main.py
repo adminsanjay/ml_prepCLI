d45d6f0e7f636a6a7f981344fe506e4874030cbb
adminsanjay <sanjay112811@gmail.com>
2026-05-16 23:33:51 +0530

    Main

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Ho")
+# pyrefly: ignore [missing-import]
+from termcolor import colored
+'''from data_description import DataDescription
+from data_input import DataInput
+from imputation import Imputation
+from download import Download
+from categorical import Categorical
+from feature_scaling import FeatureScaling
+class Preprocessor:
+    tasks=[
+        '1. Data Description',
+        '2. Handling NULL Values',
+        '3. Encoding Categorical Data',
+        '4. Feature Scaling of the Dataset',
+        '5. Download the modified dataset'
+    ]
+    data = 0
+    def __init__(self):
+        self.data = DataInput().get_data()
+        print("\n\n"+colored("Data loaded successfully to preprocessor CLI 😃",attrs=["bold"],color="green")+ "\n\n")'''
+print(f"\U0001F440")
+
+        
+
