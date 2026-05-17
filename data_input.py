a454dd9597cc978e10288f589aaf68dc18dcf737
adminsanjay <sanjay112811@gmail.com>
2026-05-17 15:13:45 +0530

    data_input added

--- data_input.py
+++ data_input.py
@@ -1,25 +1,26 @@
 from os import path
 import sys
 import pandas as pd
-# argv.py
+
+
 class DataInput:
+
     
     bold_start = "\033[1m"
     bold_end = "\033[0;0m"
 
-    # all extensions supported by this project.
+    
     supported_file_extensions = [
         '.csv',
     ]
 
-    # function to convert all the column names of any specific dataset into lowercase.
+
     def change_to_lower_case(self, data):
         for column in data.columns.values:
             data.rename(columns = {column : column.lower()}, inplace = True)
         return data
 
-    # function that takes any dataset from the input file and convert it into DataFrame.
-    # The print statements are well defined and tells about the state of the errors.
+
     def get_data(self,file):
         try:
             filename,file_extension=path.splitext(file)
